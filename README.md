f61286124a28ccc3df12b1185c44d364a09ece02
Lennon Manchester <lennon.manchester@walmart.com>
2016-06-02 20:38:14 -0300

    Added hangmang clojure project to git

---
+++ README.md
@@ -0,0 +1,19 @@
+# Hangman
+
+[Hangman games](https://en.wikipedia.org/wiki/Hangman_(game))
+
+
+## Usage
+
+`$ java -jar hangman-0.1.0-standalone.jar`
+
+## Options
+
+FIXME: listing of options this app accepts.
+
+## License
+
+Copyright © 2015
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ src/hangman/core.clj
@@ -0,0 +1,53 @@
+(ns hangman.core
+  (:gen-class))
+
+(def lives-total 6)
+(def secret-word "WATERMELON")
+
+(defn lose [] (print "You lose"))
+(defn won [] (print "You won"))
+
+(defn missing-letters [word hits]
+  (remove (fn [letter] (contains? hits (str letter))) word)
+)
+
+(defn guessed-word? [word hits]
+  (empty? (missing-letters word hits))
+)
+
+(defn read-letter![](read-line))
+
+(defn hit? [guess word] (.contains word guess))
+
+(defn print-hang
+  [lives words hits]
+  (println "Lives " lives)
+  (doseq [letter (seq words)]
+    (if
+      (contains? hits (str letter))
+        (print letter " ")
+        (print "_" " ")))
+  (println))
+
+(defn game
+  [lives word hits]
+  (print-hang lives word hits)
+  (cond
+    (= lives 0) (lose)
+    (guessed-word? word hits) (won)
+    :else
+    (let [guess (read-letter!)]
+      (do
+        (if (hit? guess word)
+          (do
+            (println "You have guessed a letter!")
+            (recur lives word (conj hits guess)))
+          (do
+            (println "Wrong letter! You have missed a life!")
+            (recur (dec lives) word hits)))))))
+
+(defn start-game [] (game lives-total secret-word #{}))
+
+(defn -main
+  [& args]
+  (start-game))
